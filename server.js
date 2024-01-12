96d7a6ca3ed4eb8c122abfe685072e6cc3850d7e
racied92 <123591703+racied92@users.noreply.github.com>
2024-01-12 10:38:58 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,24 @@
-// Importar el módulo json-server
+// JSON Server module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
+
+// Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
-// Middleware para parsear el cuerpo de la solicitud como JSON
-server.use(jsonServer.bodyParser);
-
-// Agregar una nueva tarea
-server.post("/tasks", (req, res) => {
-  try {
-    const newTask = req.body;
-    newTask.id = Date.now(); // Asignar un ID único basado en la marca de tiempo
-    db.tasks.push(newTask); // 'db' es el objeto que representa tu base de datos
-    res.json(newTask);
-  } catch (error) {
-    console.error("Error al agregar tarea:", error);
-    res.status(500).send("Error interno del servidor");
-  }
-});
-
-// Middleware predeterminado y enrutador
 server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/*": "/$1",
+ })
+);
 server.use(router);
-
-// Escuchar en el puerto 3000
+// Listen to port
 server.listen(3000, () => {
-  console.log("JSON Server is running on port 3000");
+ console.log("JSON Server is running");
 });
+
+// Export the Server API
+module.exports = server;
